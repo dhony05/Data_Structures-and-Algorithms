db2a6e6acce5ea9842b726caa6d5cffb9fc5aa8f
Donelys Familia <donelysprivate@gmail.com>
2019-08-29 22:21:28 -0400

    adding notes

--- src/ADT/App.java
+++ src/ADT/App.java
@@ -6,8 +6,10 @@ public class App {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter("Bill");
-		for (int i = 0; i < "bill".length(); i++) {
+		for (int i = 0; i < "bill".length(); i++) {  
 			counter.increment();
+			// Lineral growth rate algorith which will iterate the amount of time as the imput
+			
 		}
 		
 		System.out.println(counter.getCurrentValue());
