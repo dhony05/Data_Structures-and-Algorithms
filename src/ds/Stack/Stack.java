4dc5741ddcce0efa4de5c266bd063fc8ad4b5a50
Donelys Familia <donelysprivate@gmail.com>
2019-08-31 20:01:40 -0400

    adding stack

---
+++ src/ds/Stack/Stack.java
@@ -0,0 +1,47 @@
+package ds.Stack;
+
+public class Stack {
+	
+	private int maxSize;
+	
+	private long[] stackArray;
+	
+	private int top; // return the last value in the stack
+	
+	
+	public Stack(int size) {
+		// maxSize is going to be the size that will be pass to this
+		this.maxSize = size;
+		this.stackArray = new long[maxSize];
+		this.top = -1;
+		
+	}
+	
+	public void push(long value) {
+		// this method takes a value which is going to be added to the stack
+		// and the top will be incremented
+		top++;
+		stackArray[top] = value;
+		
+		
+	}
+	public long pop() {
+		//this method will remove the element that is at the top of the array
+		//Then will set a new top value;
+		int old_top = top;
+		top -- ;
+		return stackArray[old_top];
+	}
+	
+	public long peak() {
+		//This method will return the value at the top in the array
+		return stackArray[top];
+	}
+	
+	public boolean isEmpty() {
+		//This method will return true if the stack is empty
+		return (top == -1);
+		
+	}
+
+}
