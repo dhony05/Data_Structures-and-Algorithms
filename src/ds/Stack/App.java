f05b4335f33fcebfac2d0a47df0e1ebb4b405efa
Donelys Familia <donelysprivate@gmail.com>
2019-08-31 23:47:55 -0400

    trying to configurate a new and flexible stack

---
+++ src/ds/Stack/App.java
@@ -0,0 +1,38 @@
+package ds.Stack;
+
+public class App {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Stack stack = new Stack(2);
+		
+		stack.push(50);
+		stack.push(13);
+		stack.push(24);
+		
+		stack
+	
+	
+		
+		
+		while(!stack.isEmpty()) {
+			long value = stack.pop();
+			System.out.println(value);
+			Thread.sleep(2000);
+			
+		}
+		while(!flexibleStack.isEmpty()) {
+			long value = stack.pop();
+			System.out.println(value);
+			Thread.sleep(2000);
+			
+		}
+		
+		
+	}
+	public static String reverseString(String str){
+		return str;
+	}
+	
+	
+
+}
--- src/ds/Stack/Stack.java
+++ src/ds/Stack/Stack.java
@@ -9,28 +9,39 @@ public class Stack {
 	private int top; // return the last value in the stack
 	
 	
-	public Stack(int size) {
+	public Stack(int size){
 		// maxSize is going to be the size that will be pass to this
-		this.maxSize = size;
-		this.stackArray = new long[maxSize];
-		this.top = -1;
+	
+			this.maxSize = size;
+			this.stackArray = new long[maxSize];
+			this.top = -1;
+	
 		
 	}
 	
 	public void push(long value) {
 		// this method takes a value which is going to be added to the stack
 		// and the top will be incremented
+		if(isFull())  {
+			System.out.println("This stack is full");
+			
+		}else {
 		top++;
 		stackArray[top] = value;
-		
+		}	
 		
 	}
 	public long pop() {
-		//this method will remove the element that is at the top of the array
+		//This method will remove the element that is at the top of the array
 		//Then will set a new top value;
+		if(isEmpty())  {
+			System.out.println("This stack is full");
+			return -1;
+		}else {
 		int old_top = top;
 		top -- ;
 		return stackArray[old_top];
+		}
 	}
 	
 	public long peak() {
@@ -42,6 +53,12 @@ public class Stack {
 		//This method will return true if the stack is empty
 		return (top == -1);
 		
+	}
+	
+	public boolean isFull() {
+		//This method will return true if the maximum size -1 is equal to top
+		//have to remember top represent the index position of items 
+		return (maxSize -1 == top);
 	}
 
 }
